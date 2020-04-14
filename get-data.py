e6bb245543acf9fd6309ce5da7901e7b81df63a9
mik1893 <michele.roscelli@gmail.com>
2020-04-14 13:17:29 +0100

    add recursivity

--- get-data.py
+++ get-data.py
@@ -2,6 +2,7 @@ import requests
 import json
 import pandas as pd
 import numpy
+from datetime import datetime
 from ratelimit import limits, sleep_and_retry
 
 # static headers with Riot Token
@@ -18,11 +19,11 @@ MATCH_NR = 20 # number of matches to retrieve
 RECURSION_DEGREE = 2 # number of hops across matches (e.g. get all participants of from all the matches of the participants of your matches = 2nd degree)
 API_CALL_LIMIT = 100
 API_CALL_PERIOD = 120
+SUMMONER_NAME = "Holysh3it"
 
 # QUEUE IDS
 RANKED = 1100
 NORMAL = 1090
-
 
 #################### Rate-limited request.get ####################
 @sleep_and_retry
@@ -44,17 +45,19 @@ def get_data(summoners, degree):
         degree = degree + 1
         if degree <= RECURSION_DEGREE:
             for summoner in summoners:
-                puuid_url = "https://euw1.api.riotgames.com/tft/summoner/v1/summoners/by-name/"+summoner
-                puuid = json.loads(call_api(puuid_url).text)['puuid']
-                matches_url = "https://europe.api.riotgames.com/tft/match/v1/matches/by-puuid/"+puuid+"/ids?count="+str(MATCH_NR)
+                matches_url = "https://europe.api.riotgames.com/tft/match/v1/matches/by-puuid/"+summoner+"/ids?count="+str(MATCH_NR)
                 matches = json.loads(call_api(matches_url).text)
 
                 for match in matches:
                     sinergies, matchdata = get_info(match)
-                    SINERGIES.append(sinergies)
-                    MATCHDATA.append(matchdata)
-                    summ_list = matchdata["puuid"].unique().tolist()
-                    get_data(summ_list, degree)
+                    if sinergies is None or matchdata is None:
+                        pass
+                    else: 
+                        SINERGIES.append(sinergies)
+                        MATCHDATA.append(matchdata)
+                        summ_list = matchdata["puuid"].unique().tolist()
+                        summ_list.remove(summoner) # drop the player itself from the list as already retrieved - TODO: SKIP MATCHES ALREADY RETRIEVED!!
+                        get_data(summ_list, degree)
 
     except Exception as e:
         print("GetData - Error: "+str(e))
@@ -67,14 +70,17 @@ def get_info(match):
         sinergies = []
         matchdata = []
         if match_info['info']['queue_id'] == RANKED and match_info['info']['tft_set_number'] == 3 :
+            match_datetime = datetime.fromtimestamp(round(match_info['info']['game_datetime']/1000,0))
             for participant in match_info['info']['participants']:
                 # loop through participants and start generating the dataset
                 df = pd.json_normalize(participant["traits"])
                 df["puuid"] = participant["puuid"]
                 df["match"] = match
+                df["match_datetime"] = match_datetime
+                #df["game_datetime"] = datetime.fromtimestamp(match_info["info"]["game_datetime"])
                 sinergies.append(df)
-                ptcp = pd.DataFrame(columns=["match","puuid","level","placement","last_round"])
-                ptcp.loc[0] = [match,participant["puuid"],participant["level"],participant["placement"],participant["last_round"]]
+                ptcp = pd.DataFrame(columns=["match","puuid","level","placement","last_round","match_datetime"])
+                ptcp.loc[0] = [match,participant["puuid"],participant["level"],participant["placement"],participant["last_round"],match_datetime]
                 matchdata.append(ptcp)
 
             matchdata = pd.concat(matchdata)
@@ -84,15 +90,19 @@ def get_info(match):
             return None, None
     except Exception as e:
         print("GetInfo - Error: "+str(e))
+        return None, None
 
 
 # Main Thread
 if  __name__ == "__main__":
     
-    # start with one name and 0 degree degree recursion
-    get_data(["Holysh3it"],0)
+    # first puuid i need to get via API from summoner name
+    puuid_url = "https://euw1.api.riotgames.com/tft/summoner/v1/summoners/by-name/"+SUMMONER_NAME
+    summoner = json.loads(call_api(puuid_url).text)['puuid']
+    #print(summoner)
+    # start with one name and 0 degree recursion
+    get_data([summoner],0)
 
-        
     SINERGIES = pd.concat(SINERGIES)
     SINERGIES.reset_index(drop=True, inplace=True)
     SINERGIES.set_index(["match","puuid","name"],inplace=True)
